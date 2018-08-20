592c75d81ec8d97f478bcb049b6fb410b28b91a5
ronak <knockronak@gmail.com>
2018-08-19 21:19:51 -0400

    styling home page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,52 @@
 var friendsArray = [
     {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
-]
+        "name": "Jerry",
+        "photo": "https://i.pinimg.com/originals/9b/a2/57/9ba25796112cad616be27e473ae1e149.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Matt",
+        "photo": "https://www.thoughtco.com/thmb/5vJt0OCkHbCSw95QZ9KpNq2hiJs=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/bart-simpson-58fe1f773df78ca159b60cc2.jpg",
+        "scores": [
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "5",
+            "1",
+            "1",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "old guy",
+        "photo": "https://mytebox.com/photos/134/Fred-Flintstone-cartoon-characters.webp",
+        "scores": [
+            "1",
+            "3",
+            "2",
+            "5",
+            "3",
+            "3",
+            "1",
+            "1",
+            "2",
+            "1"
+        ]
+    }
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,6 @@
 var path = require('path');
 var friendsData = require("../data/friends");
 
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -19,51 +18,32 @@ module.exports = function(app){
 		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
 		// It will do this by sending out the value "true" have a table
 			// console.log(friendsData);
-			res.json(req.body); // KEY LINE
+			// res.json(req.body); // KEY LINE
 
 			var newAns = req.body.scores;
 			console.log('newAns: ', newAns);
-			var newArray = [ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ];
-			console.log(calcScore(newAns, newArray));
-			// var check = newAns.map(function(item, index){
-			// 	return Math.abs(newArray[index] - item)		
-			// })
-
-			// console.log('check: ', check);
-			// const reducer = (accumulator, currentValue) => accumulator + currentValue;
-			// check.reduce(reducer);
-			// console.log(check.reduce(reducer));
-			// calcScore(newAns, newArray);
+;
 
 			function calcScore(inputProfile, allProfiles){
 				var check = inputProfile.map(function(item, index){
 					return Math.abs(allProfiles[index] - item)		
-				})
-
-				// console.log('check: ', check);
+				});
 				const reducer = (accumulator, currentValue) => accumulator + currentValue;
-				console.log(check.reduce(reducer));
 				return check.reduce(reducer);
 			}
 
 			console.log("----------------------");
-			function getMatchingProfile(){
-				var maxScore = 0;
-				var matchingProfile;
-				for (var eachScore in friendsData){
-					if (calcScore(newAns,friendsData[eachScore].scores) > maxScore){
-						maxScore = calcScore(newAns,friendsData[eachScore].scores)
-						matchingProfile = friendsData[eachScore]
-					}
-				}
-				// console.log(maxScore);
-				return matchingProfile;
-			};
-			console.log(getMatchingProfile());
+			var scorecard = [];
+			for (var eachScore in friendsData){
+				scorecard.push(calcScore(newAns,friendsData[eachScore].scores));
+			}
+			console.log("Scorecard" , scorecard);
+			console.log(scorecard.indexOf(Math.min(...scorecard)));
+			var matchingProfileIndex = scorecard.indexOf(Math.min(...scorecard));
 			friendsData.push(req.body);
-
-
-	});
+			console.log()
+        	res.json(friendsData[matchingProfileIndex]);
+    });
 
 
 	app.post('/api/clear', function(req, res){
@@ -73,4 +53,4 @@ module.exports = function(app){
 
 		console.log(tableData);
 	})
-}
+};
